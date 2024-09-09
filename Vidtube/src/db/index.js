bcdf1b18499ab488824c9ae12dc001e81cf2eb48
tejaspatil1936 <tejaspatil1936@gmail.com>
2024-09-09 12:01:08 +0530

    codes

--- Vidtube/src/db/index.js
+++ Vidtube/src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async ( ) => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB host: ${connectionInstance.connection.host}`);
+        console.log(`\nMongoDB connected !! DB host: ${connectionInstance.connection.host}`);
         // console.log(connectionInstance);
     } catch (error) {
         console.log("MONGODB connection error ", error)
--- Vidtube/src/index.js
+++ Vidtube/src/index.js
@@ -2,12 +2,21 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
+import app from "./app.js"
  
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed !!! ", err);
+})
 
 
 
