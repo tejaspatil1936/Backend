0201b55a229698d235de26dc86f3333ea9157b88
tejaspatil1936 <tejaspatil1936@gmail.com>
2024-09-12 23:10:39 +0530

    updated user controller

--- Vidtube/src/controllers/user.controller.js
+++ Vidtube/src/controllers/user.controller.js
@@ -2,10 +2,9 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler( async (req, res) => {
-    console.log('registerUser called', req.body);
-    res.status(200).json({
-        message: "hello tejas"
-    })
+   
+    const {username, fullname } = req.body
+    console.log("username: ", username);
 
 })
 
