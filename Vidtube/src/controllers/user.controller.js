300a8952477552a1a731dce9154787abbdee26de
tejaspatil1936 <tejaspatil1936@gmail.com>
2024-09-13 03:38:29 +0530

    register user

--- Vidtube/src/controllers/user.controller.js
+++ Vidtube/src/controllers/user.controller.js
@@ -1,11 +1,15 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser = asyncHandler( async (req, res) => {
    
-    const {username, fullname } = req.body
+    const {fullname, email, username, password} = req.body
     console.log("username: ", username);
 
+    if([fullname, email, username, password].some((field) => field?.trim() === ""))
+    {
+        throw new ApiError(400, "all fields are compulsory")
+    }
 })
 
 export {registerUser}
